036498675b327af329de75452741d68f65869199
raywan-110 <545981160@qq.com>
2021-01-15 15:55:32 +0800

    change path in DB

--- src/DB.py
+++ src/DB.py
@@ -5,8 +5,8 @@ from __future__ import print_function
 import pandas as pd
 import os
 
-DB_dir = 'database'
-DB_csv = 'data.csv'
+DB_dir = '..\\oxbuild_images'
+DB_csv = '..\\oxbuild_images.csv'
 
 # 建立图像数据库的索引文件
 class Database(object):
