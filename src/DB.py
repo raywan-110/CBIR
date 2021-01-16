02a0c275ce244508eb5e05e5a805528081fb98e9
raywan-110 <545981160@qq.com>
2021-01-16 17:35:28 +0800

    modify DB

--- src/DB.py
+++ src/DB.py
@@ -5,20 +5,26 @@ from __future__ import print_function
 import pandas as pd
 import os
 
-DB_dir = '..\\oxbuild_images'
-DB_csv = '..\\oxbuild_images.csv'
+# DB_dir = '..\\oxbuild_images'
+# DB_csv = '..\\oxbuild_images.csv'
+
+DB_dir = '..\\database'
+DB_csv = '..\\database.csv'
+
 
 # 建立图像数据库的索引文件
 class Database(object):
-    def __init__(self):
+    def __init__(self, db_csv='..\\oxbuild_images.csv', db_dir='..\\oxbuild_images.csv'):
+        self.db_csv = db_csv
         self._gen_csv()
-        self.data = pd.read_csv(DB_csv)
+        self.db_dir = db_dir
+        self.data = pd.read_csv(db_csv)
         self.classes = set(self.data["cls"])
 
     def _gen_csv(self):
-        if os.path.exists(DB_csv):
+        if os.path.exists(self.db_csv):
             return
-        with open(DB_csv, 'w', encoding='UTF-8') as f:
+        with open(self.db_csv, 'w', encoding='UTF-8') as f:
             f.write("img,cls")
             for root, _, files in os.walk(DB_dir, topdown=False):
                 cls = root.split('\\')[-1]  # windows环境下分隔符需要修改为\\形式
@@ -39,7 +45,7 @@ class Database(object):
 
 
 if __name__ == "__main__":
-    db = Database()
+    db = Database(db_csv=DB_csv, db_dir=DB_dir)
     data = db.get_data()
     classes = db.get_class()
 
